525f1bc1dc78c1a6fe833c5deb7cad43db36cc87
MrLYC <imyikong@gmail.com>
2016-11-06 01:02:39 +0800

    update

--- entry.sh
+++ entry.sh
@@ -7,10 +7,14 @@ fi
 exec client \
     --localaddr "${LOCALADDR}" \
     --remoteaddr "${REMOTEADDR}" \
+    --key "${KEY}" \
+    --crypt "${CRYPT}" \
     --mode "${MODE}" \
     --conn "${CONN}" \
     --autoexpire "${AUTOEXPIRE}" \
     --mtu "${MTU}" \
     --sndwnd "${SNDWND}" \
     --rcvwnd "${RCVWND}" \
+    --datashard "${DATASHARD}" \
+    --parityshard "${PARITYSHARD}" \
     --dscp "${DSCP}"
