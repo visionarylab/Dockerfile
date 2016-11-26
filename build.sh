50fc00196d158cc0065a84b7655f4b5e478f6fe7
MrLYC <imyikong@gmail.com>
2016-11-27 03:24:06 +0800

    fix extra bug

--- build.sh
+++ build.sh
@@ -8,8 +8,9 @@ apk add wget git ctags zip
 
 mkdir -p /tmp/opengrok
 cd /tmp/opengrok
-wget -O opengrok.tar.gz https://github.com/OpenGrok/OpenGrok/files/467358/opengrok-0.12.1.6.tar.gz
-tar -zxvf opengrok.tar.gz -C /var
-rm opengrok.tar.gz
+wget -O opengrok.tar.gz.zip https://github.com/OpenGrok/OpenGrok/files/467358/opengrok-0.12.1.6.tar.gz.zip
+unzip opengrok.tar.gz.zip
+tar -zxvf opengrok-*.tar.gz -C /var
+rm opengrok*
 
 rm -rf /var/cache/apk/*
