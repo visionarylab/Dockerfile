cf560891f0c4889082fad11b05b42ca203e37c8d
MrLYC <imyikong@gmail.com>
2016-11-27 03:44:13 +0800

    bug fixes

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ mkdir -p /tmp/opengrok
 cd /tmp/opengrok
 wget -O opengrok.tar.gz.zip https://github.com/OpenGrok/OpenGrok/files/467358/opengrok-0.12.1.6.tar.gz.zip
 unzip opengrok.tar.gz.zip
-tar -zxvf opengrok.tar.gz -C /var
+tar -zxvf opengrok-*.tar.gz -C /var
 
 mv /var/opengrok* /var/opengrok
 export PATH=/var/opengrok/bin:$PATH
