375f1e825753081475d66d40d76b0e6770e4e1e8
MrLYC <imyikong@gmail.com>
2016-11-27 12:10:07 +0800

    add async mode

--- entry.sh
+++ entry.sh
@@ -8,45 +8,58 @@ rm -rf /var/opengrok/src
 mkdir -p "${SOURCE_DIR}"
 ln -s "${SOURCE_DIR}" /var/opengrok/src
 
-/opt/tomcat/bin/catalina.sh start
+indexing_source() {
+    echo "indexing"
+    /var/opengrok/bin/OpenGrok index "${SOURCE_DIR}"
+}
 
-get_git_source() {
+indexing_git_source() {
     for i in $(echo "${GIT_SOURCE}" | tr ";" "\n"); do
-        git clone "$i" "${SOURCE_DIR}"
+        git clone --depth=1 "$i" "${SOURCE_DIR}"
+        indexing_source
     done
 }
 
-get_tar_source() {
+indexing_tar_source() {
     for i in $(echo "${TAR_SOURCE}" | tr ";" "\n"); do
         wget -O /tmp/source.tar $i
         tar -C "${SOURCE_DIR}" -xvf /tmp/source.tar
+        indexing_source
     done
     [ -e /tmp/source.tar ] && rm /tmp/source.tar
 }
 
-get_tgz_source() {
+indexing_tgz_source() {
     for i in $(echo "${TGZ_SOURCE}" | tr ";" "\n"); do
         wget -O /tmp/source.tar.gz $i
         tar -C "${SOURCE_DIR}" -xvf /tmp/source.tar.gz
+        indexing_source
     done
     [ -e /tmp/source.tar.gz ] && rm /tmp/source.tar.gz
 }
 
-get_zip_source() {
+indexing_zip_source() {
     for i in $(echo "${ZIP_SOURCE}" | tr ";" "\n"); do
         wget -O /tmp/source.zip $i
         unzip -d "${SOURCE_DIR}" /tmp/source.zip
+        indexing_source
     done
     [ -e /tmp/source.zip ] && rm /tmp/source.zip
 }
 
-get_git_source &
-get_tar_source &
-get_tgz_source &
-get_zip_source &
+init_source() {
+    indexing_source
+    indexing_git_source
+    indexing_tar_source
+    indexing_tgz_source
+    indexing_zip_source
+}
 
-while true; do
-    echo "indexing"
-    /var/opengrok/bin/OpenGrok index "${SOURCE_DIR}"
-    sleep "${INDEX_DELAY}"
-done
+
+if [ "${ASYNC_MODE}" = "0" ]; then
+    init_source
+else
+    init_source &
+fi
+
+/opt/tomcat/bin/catalina.sh run
