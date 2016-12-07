b2d273e8e1260cb0123137f0950133ccaead9665
MrLYC <imyikong@gmail.com>
2016-12-07 22:14:25 +0800

    init

--- build.sh
+++ build.sh
@@ -1,7 +1,16 @@
-#!/bin/bash
+#!/bin/sh
 
 set -x
-apk update
 
+export GOPATH="/go"
+mkdir -p ${GOPATH}
+
+apk update
+apk add git go mysql-client
+go get github.com/lisijie/webcron
+apk clean
+
+apk del go git
+rm -rf ${GOPATH}
 rm -rf /var/cache/apk/*
 rm -f /build.sh || true
